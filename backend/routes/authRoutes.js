47ec2799e08fb6c140378720c06c07146d7e0cf5
Harsh Kanjiya <harshkanjiya001@gmail.com>
2023-01-29 15:18:06 +0530

    maintainance

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -4,6 +4,7 @@ const passport = require('passport');
 const router = express.Router()
 
 
+
 router.route('login/success').get(async (res, res) => {
     if (req.user) {
         res.status(200).json({ success: true, user: req.user })
